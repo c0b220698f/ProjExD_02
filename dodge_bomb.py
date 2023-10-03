803eb5fb0562d994690a8d28cad044202d94ab53
c0b220698f <c0b220698f@edu.teu.ac.jp>
2023-10-03 15:28:41 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -51,7 +51,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-
+        if kk_rct.colliderect(bd_rct):
+            print("Game Over")
+            return
         screen.blit(bg_img, [0, 0])
         """こうかとん"""
         key_lst = pg.key.get_pressed()
